19d2304e3e9679c61e6ab95034cb971f80127987
Rodrigo <cesar.dominguez.5021@gmail.com>
2026-02-18 21:47:21 -0300

    Creacion

---
+++ src/main/java/com/proyecto/forohub/model/Topico.java
@@ -0,0 +1,23 @@
+package com.proyecto.forohub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "topicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String mensaje;
+
+}
